41d47ffb984051762cf814e7ab5096b60d520301
JYCabello <j.cabello.ramos@gmail.com>
2023-02-16 16:08:45 +0100

    entity

---
+++ src/entities/post.ts
@@ -0,0 +1,19 @@
+import { Entity, Reduces } from '@boostercloud/framework-core'
+import { UUID } from '@boostercloud/framework-types'
+import { PostCreated } from '../events/post-created'
+
+@Entity
+export class Post {
+  public constructor(
+    public id: UUID,
+    readonly title: string,
+    readonly content: string,
+    readonly author: string,
+  ) {}
+
+  @Reduces(PostCreated)
+  public static reducePostCreated(event: PostCreated, currentPost?: Post): Post {
+    return new Post(event.postId, event.title, event.content, event.author)
+  }
+
+}
